ef9a666e999b4dfa4c9227cfaf7cd8d9b3db268a
aidan <aidankhogg@gmail.com>
2025-03-04 22:26:50 +0000

    blacksmith shell

--- gameforged/workshop.py
+++ gameforged/workshop.py
@@ -6,3 +6,9 @@ from gameforged.control_tower import LOG as log
 from gameforged.mechanics.players import HumanPlayer, AIPlayer, SimulatedAgent
 from gameforged.utilities.extensions import ExtensionEngine
 
+class Blacksmith:
+    """
+    Universal builder and creator
+    """
+
+    log.warning("Blacksmith class not implemented.")
